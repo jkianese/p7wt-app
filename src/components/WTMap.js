1de9be56223a5abd815639e63d594f4bee9c8c31
[jkianese] <[jkianese@gmail.com]>
2018-10-03 14:30:27 -0400

    push new component

---
+++ src/components/WTMap.js
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+// import './App.css';
+import axios from 'axios';
+
+class App extends Component {
+  
+    state = {
+      venues: []
+    }
+  
+    componentDidMount() {
+      this.getVenues()
+      // this.loadMap()
+    }
+  
+    loadMap = () => { // script on index.html. Can I ref that?
+      loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ&callback=initMap")
+      window.initMap = this.initMap
+    }
+
+    getVenues = () => {
+      const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+      const parameters = {
+        client_id: "F1BIU3KU3RZFBQKCLKJ2MX1AT2ZRZFYRUXTJUMFGA1YUS5ZF",
+        client_secret: "N1HLP0MSWYJJATAS3CBQTTSZ2WLME5RB2TAUWHGE2UXZ5A1E",
+        query: "arts, food",
+        near: "Pittsburgh",
+        v: "20181012"
+      }
+      // npm install axios to run
+      axios.get(endPoint + new URLSearchParams(parameters))
+        .then(response => {
+          this.setState({
+            venues: response.data.response.groups[0].items
+          }, this.loadMap())
+        })
+        .catch(error => {
+          console.log("Error: " + error)
+        })
+    }
+
+    initMap = () => {
+      // create a map 
+      const map = new window.google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.441632, lng: -80.008031},
+        zoom: 13
+      })
+      
+      // create an infowindow
+    var infowindow = new window.google.maps.InfoWindow()
+
+    // display dynamic markers
+    this.state.venues.map(myVenue => {
+
+    var contentString = `${myVenue.venue.name}` // Removed Place Name in WT 
+
+    var marker = new window.google.maps.Marker({
+      position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
+      map: map,
+      title: myVenue.venue.name
+    })
+
+    // click on a marker
+    marker.addListener('click', function() {
+      
+     // Change the Content
+     infowindow.setContent(contentString)
+     
+     // Open an InfoWindow
+     infowindow.open(map, marker)
+    })
+  });  
+      
+}      
+
+  render () {
+    return (
+      <main>
+        <div id="map">
+          
+        </div>
+      </main>  
+    )
+  }
+}
+
+function loadScript(source) {
+  var index = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = source
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+
+export default App
