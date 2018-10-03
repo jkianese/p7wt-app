debe3228dbfa30f6d4974f6e9f1b8976ac2ee9da
[jkianese] <[jkianese@gmail.com]>
2018-10-03 14:28:12 -0400

    push new component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import axios from 'axios';
+import WTMap from './components/WTMap'
 
 class App extends Component {
-  render() {
+  
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <main>
+        <div id="map">
+          <WTMap />         
+        </div>
+      </main>  
+    )
   }
 }
-
-export default App;
+export default App
